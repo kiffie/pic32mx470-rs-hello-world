58bbf98104c3b8e3e4b624031c602a7ab4002462
Kiffie van Haash <kiffie@users.noreply.github.com>
2019-03-02 11:17:35 +0100

    added support for formatted output by implementing core::fmt::Write

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@
 #![feature(asm)]
 
 use core::panic::PanicInfo;
+use core::fmt::Write;
 use mips_rt;
-//use volatile_register::{RW, RO};
 use pic32mx4xxfxxxh;
 
 
@@ -36,43 +36,59 @@ fn set_green_led(on: bool){
     }
 }
 
-fn uart2_init() {
-    let  p = unsafe { pic32mx4xxfxxxh::Peripherals::steal()};
+struct Uart;
 
-    let pps = p.PPS;
-    pps.rpf5r.write(|w| unsafe { w.rpf5r().bits(0b0001) }); // UART 2 on RPF5
+impl Uart {
 
-    let sys_clock : u32 = 96000000;
-    let pb_clock : u32 = sys_clock; // TODO: consider PBDIV
-    let baud_rate : u32 = 115200;
-    let brg = (pb_clock/(4*baud_rate)-1) as u16;
+    fn init() -> Uart {
+        let  p = unsafe { pic32mx4xxfxxxh::Peripherals::steal()};
 
-    let uart2 = p.UART2;
-    uart2.u2mode.write(|w|  { w.brgh().bit(true) });
-    uart2.u2sta.write(|w| unsafe { w.utxen().bit(true).utxisel().bits(0b10) });
-    uart2.u2brg.write(|w| unsafe { w.brg().bits(brg) });
-    uart2.u2modeset.write(|w| w.on().bit(true));
+        let pps = p.PPS;
+        pps.rpf5r.write(|w| unsafe { w.rpf5r().bits(0b0001) }); // UART 2 on RPF5
+
+        let sys_clock : u32 = 96000000;
+        let pb_clock : u32 = sys_clock; // TODO: consider PBDIV
+        let baud_rate : u32 = 115200;
+        let brg = (pb_clock/(4*baud_rate)-1) as u16;
+
+        let uart2 = p.UART2;
+        uart2.u2mode.write(|w|  { w.brgh().bit(true) });
+        uart2.u2sta.write(|w| unsafe { w.utxen().bit(true).utxisel().bits(0b10) });
+        uart2.u2brg.write(|w| unsafe { w.brg().bits(brg) });
+        uart2.u2modeset.write(|w| w.on().bit(true));
+        Uart
+    }
+
+    fn write_byte(&self, byte: u8){
+
+        let  uart2 = unsafe { pic32mx4xxfxxxh::Peripherals::steal()}.UART2;
+
+        while uart2.u2sta.read().utxbf().bit() { }
+        uart2.u2txreg.write(|w| unsafe { w.u2txreg().bits(byte as u16) });
+    }
 }
 
-fn uart2_write(byte: u8){
+impl Write for Uart {
 
-    let  uart2 = unsafe { pic32mx4xxfxxxh::Peripherals::steal()}.UART2;
+    fn write_str(&mut self, s: &str) -> core::fmt::Result {
+        for b in s.bytes() {
+            self.write_byte(b);
+        }
+        Ok(())
+    }
 
-    while uart2.u2sta.read().utxbf().bit() { }
-    uart2.u2txreg.write(|w| unsafe { w.u2txreg().bits(byte as u16) });
 }
-
 
 #[no_mangle]
 pub unsafe extern "C" fn main() -> ! {
 
     let txt = "Hello World!\n";
-    uart2_init();
+    let mut uart = Uart::init();
     let mut state = false;
+    let mut ctr : u32 = 0;
     loop {
-        for ch in txt.bytes() {
-            uart2_write(ch);
-        }
+        writeln!(uart, "Hello World! ctr = {}", ctr).unwrap();
+        ctr += 1;
         set_yellow_led(state);
         set_green_led(!state);
         let mut i = 1000000;
@@ -84,13 +100,8 @@ pub unsafe extern "C" fn main() -> ! {
     }
 }
 
-
-
 #[panic_handler]
 fn panic(_panic: &PanicInfo<'_>) -> ! {
-    let x = 42;
     loop {}
 }
-
-#[lang = "eh_personality"] extern fn rust_eh_personality() {}
 
